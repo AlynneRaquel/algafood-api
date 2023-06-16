fb564b99907bd03f3052f80d399f0964c4e14ad3
Alynne Raquel <alynneraquel@homail.com>
2023-06-16 12:31:15 -0300

    Publicando e consumindo eventos customizados @EventListener

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,56 +1,22 @@
 package com.algaworks.algafood.di.service;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
-import com.algaworks.algafood.di.notificacao.NivelUrgencia;
-import com.algaworks.algafood.di.notificacao.Notificador;
-import com.algaworks.algafood.di.notificacao.TipoDoNotificador;
 
-//@Component
+@Component
 public class AtivacaoClienteService {
-	
-	@TipoDoNotificador (NivelUrgencia.SEM_URGENCIA)
-	@Autowired//ponto de injeção 3 //(required = false)indica que é opcional ter uma dependência 
-	private Notificador notificador;
-	
-//	@PostConstruct
-	public void init() {
-		System.out.println("INIT" + notificador);
-	}
-	
-//	@PreDestroy
-	public void destroy() {
-		System.out.println("DESTROY");
-	}
+		
+	@Autowired
+	private ApplicationEventPublisher eventPublisher;
 	
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 		
-		notificador.notificar(cliente, "Seu cadastro está ativo!");
-		
+		//dizer para o container que o cliente está ativo neste momento
+		eventPublisher.publishEvent(new ClienteAtivadoEvent(cliente));
 	}
-	
-	/*
-	 * @Autowired //ponto de injeção 1
-	 * public AtivacaoClienteService(Notificador notificador) {
-	 *  this.notificador = notificador; }
-	 */
-	
-	/*
-	 * public AtivacaoClienteService(String qualquer) { 
-	 * }
-	 */
-	
-	/*
-	 * @Autowired //outra forma de injetar 2
-	 * public void setNotificador(Notificador notificador) 
-	 * { this.notificador = notificador; }
-	 */
 	
 }
