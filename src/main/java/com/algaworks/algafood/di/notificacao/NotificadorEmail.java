83243763de70b1230524bea20439a779cb114978
Alynne Raquel <alynneraquel@homail.com>
2023-06-20 17:29:18 -0300

    Acessando propriedades com @ConfigurationProperties

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.di.notificacao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -9,17 +10,13 @@ import com.algaworks.algafood.di.modelo.Cliente;
 @Component
 public class NotificadorEmail implements Notificador {
 	
-	@Value("${notificador.email.host-servidor}")
-	private String host;
-	
-	@Value("${notificador.email.porta-servidor}")
-	private Integer porta;
-
+	@Autowired
+	private NotificadorProperties notificadorProperties;
 	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		System.out.println("Host: " + host);
-		System.out.println("Porta: " + porta);
+		System.out.println("Host: " + notificadorProperties.getHostServidor());
+		System.out.println("Porta: " + notificadorProperties.getPortaServidor());
 		
 		System.out.printf("Notificando %s através de E-mail %s:  %s\n", 
 				cliente.getNome(), cliente.getEmail(), mensagem);
---
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorProperties.java
@@ -0,0 +1,25 @@
+package com.algaworks.algafood.di.notificacao;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("notificador.email")
+public class NotificadorProperties {
+	
+	private String hostServidor;
+	private Integer portaServidor;
+	
+	public String getHostServidor() {
+		return hostServidor;
+	}
+	public void setHostServidor(String hostServidor) {
+		this.hostServidor = hostServidor;
+	}
+	public Integer getPortaServidor() {
+		return portaServidor;
+	}
+	public void setPortaServidor(Integer portaServidor) {
+		this.portaServidor = portaServidor;
+	}
+}
