efa2c0a63510b0af2746cf1ea75c95f7342b6686
Alynne Raquel <alynneraquel@homail.com>
2023-06-27 20:25:18 -0300

    Excluindo um objeto do banco de dados

--- src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
+++ src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
@@ -29,5 +29,11 @@ public class CadastroCozinha {
 	public Cozinha salvar (Cozinha cozinha) {
 		return manager.merge(cozinha);
 	}
+	
+	@Transactional
+	public void remover(Cozinha cozinha) {
+		cozinha = buscar(cozinha.getId());
+		manager.remove(cozinha);
+	}
 
 }
