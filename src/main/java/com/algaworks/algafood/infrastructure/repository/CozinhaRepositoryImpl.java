75ee405d841069a3811d3106e71293e5c6bd1a30
Alynne Raquel <alynneraquel@homail.com>
2023-07-12 15:35:43 -0300

    padrão Aggregate do DDD e implementando o padrão Repository

--- src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -1,4 +1,4 @@
-package com.algaworks.algafood.jpa;
+package com.algaworks.algafood.infrastructure.repository;
 
 import java.util.List;
 
@@ -9,31 +9,35 @@ import javax.transaction.Transactional;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
 @Component
-public class CadastroCozinha {
-	
+public class CozinhaRepositoryImpl implements CozinhaRepository {
+
 	@PersistenceContext
 	private EntityManager manager;
 	
-	public List<Cozinha> listar(){
+	@Override
+	public List<Cozinha> todas(){
 		return  manager.createQuery("from cozinha", Cozinha.class)
 				.getResultList();
 	}
 	
-	public Cozinha buscar(Long id) {
+	@Override
+	public Cozinha porId(Long id) {
 		return manager.find(Cozinha.class, id);
 	}
 	
 	@Transactional
-	public Cozinha salvar (Cozinha cozinha) {
+	@Override
+	public Cozinha adicionar (Cozinha cozinha) {
 		return manager.merge(cozinha);
 	}
 	
 	@Transactional
+	@Override
 	public void remover(Cozinha cozinha) {
-		cozinha = buscar(cozinha.getId());
+		cozinha = porId(cozinha.getId());
 		manager.remove(cozinha);
 	}
-
 }
