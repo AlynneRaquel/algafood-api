a1d08fb07bd68cbbd93023ddd3032028c0a8e5b5
Alynne Raquel <alynneraquel@homail.com>
2023-07-31 17:26:48 -0300

    Implementando consultas JPQL em repositórios

--- src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -7,6 +7,8 @@ import com.algaworks.algafood.domain.model.Cozinha;
 public interface CozinhaRepository {
 	
 	List<Cozinha> listar();
+	List<Cozinha> consultarPorNome(String nome);
+	
 	Cozinha buscar(Long id);
 	Cozinha salvar(Cozinha cozinha);
 	void remover(Long id);
