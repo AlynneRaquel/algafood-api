b5399daaf224f17eb6146b412301c684ead989e9
AlynneRaquel <alynneraquel@hotmail.com>
2023-09-05 20:18:50 -0300

    Mapeando classes incorporáveis com @Embedded e @Embeddable

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,6 +15,7 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
+import com.algaworks.algafood.api.controller.Endereco;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
@@ -38,6 +40,9 @@ public class Restaurante {
 	@ManyToOne 
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
+	
+	@Embedded
+	private Endereco endereco;
 	
 	@JsonIgnore
 	@ManyToMany
