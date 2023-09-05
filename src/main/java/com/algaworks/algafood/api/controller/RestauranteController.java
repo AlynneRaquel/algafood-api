c8c782ef15e83bd4e18b41b561fe15b7cc87803a
AlynneRaquel <alynneraquel@hotmail.com>
2023-09-05 19:49:05 -0300

    Analisando o impacto do relacionamento muitos-para-muitos na REST API

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -70,7 +70,7 @@ public class RestauranteController {
         	        .findById(restauranteId).orElse(null);
 			
 			if (restauranteAtual != null) {
-				BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+				BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "formaPagamento");
 				
 				restauranteAtual = cadastroRestaurante.salvar(restauranteAtual);
 				return ResponseEntity.ok(restauranteAtual);
