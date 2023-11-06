46f68d3d8cbfef3086db0a384294d15cbbdaa28c
AlynneRaquel <alynneraquel@hotmail.com>
2023-11-06 11:58:43 -0300

    Desafio: tratando a exceção NoHandlerFoundException

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
@@ -64,7 +65,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 			EntidadeNaoEncontradaException ex, WebRequest request) {
 
 		HttpStatus status = HttpStatus.NOT_FOUND;
-		ProblemType problemType = ProblemType.ENTIDADE_NAO_ENCONTRADA;
+		ProblemType problemType = ProblemType.RECURSO_NAO_ENCONTRADO;
 		String detail = ex.getMessage();
 
 		Problem problem = createProblemBuilder(status, problemType, detail).build(); 
@@ -168,6 +169,19 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 	    return handleExceptionInternal(ex, problem, headers, status, request);
 	}
 
+	@Override
+	protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, 
+	        HttpHeaders headers, HttpStatus status, WebRequest request) {
+	    
+	    ProblemType problemType = ProblemType.RECURSO_NAO_ENCONTRADO;
+	    String detail = String.format("O recurso %s, que você tentou acessar, é inexistente.", 
+	            ex.getRequestURL());
+	    
+	    Problem problem = createProblemBuilder(status, problemType, detail).build();
+	    
+	    return handleExceptionInternal(ex, problem, headers, status, request);
+	} 
+	
 	private Problem.ProblemBuilder createProblemBuilder(HttpStatus status,
 			ProblemType problemType, String detail){
 
