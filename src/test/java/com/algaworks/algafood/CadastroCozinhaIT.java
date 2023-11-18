b48be387be00982d84795f6d3f3895fe89fa73cc
AlynneRaquel <alynneraquel@hotmail.com>
2023-11-18 16:53:03 -0300

    Implementando Testes de API com REST Assured e validando o código de status HTTP

---
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -0,0 +1,37 @@
+package com.algaworks.algafood;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.validation.ConstraintViolationException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+
+import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.service.CadastroCozinhaService;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+@SpringBootTest
+class CadastroCozinhaIT {
+	
+	@Test
+	public void deveRetornarStatus200_QuandoConsultarCozinhas() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
+		RestAssured.given()
+			.basePath("/cozinhas")
+			.port(8080)
+			.accept(ContentType.JSON)
+		.when()	
+			.get()
+		.then()
+			.statusCode(HttpStatus.OK.value());
+	}
+}
