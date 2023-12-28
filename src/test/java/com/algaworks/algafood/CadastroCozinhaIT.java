f5dccc9b4fa34505a76e25acf618157045ff27c0
AlynneRaquel <alynneraquel@hotmail.com>
2023-12-28 17:42:22 -0300

    Testando endpoint passando parâmetro de URL

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -1,11 +1,14 @@
 package com.algaworks.algafood;
 
-import org.hamcrest.Matchers;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.TestPropertySource;
 
@@ -16,11 +19,10 @@ import com.algaworks.algafood.util.DatabaseCleaner;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource("/application-test.properties")
-class CadastroCozinhaIT {
-	
+public class CadastroCozinhaIT {
+
 	@LocalServerPort
 	private int port;
 	
@@ -31,7 +33,7 @@ class CadastroCozinhaIT {
 	private CozinhaRepository cozinhaRepository;
 	
 	@BeforeEach
-	public void setup() {
+	public void setUp() {
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		RestAssured.port = port;
 		RestAssured.basePath = "/cozinhas";
@@ -42,37 +44,57 @@ class CadastroCozinhaIT {
 	
 	@Test
 	public void deveRetornarStatus200_QuandoConsultarCozinhas() {
-		RestAssured.given()
-			.port(8080)
+		given()
 			.accept(ContentType.JSON)
-		.when()	
+		.when()
 			.get()
 		.then()
 			.statusCode(HttpStatus.OK.value());
 	}
-	
+
 	@Test
 	public void deveConter2Cozinhas_QuandoConsultarCozinhas() {
-		RestAssured.given()
-			.port(8080)
+		given()
 			.accept(ContentType.JSON)
-		.when()	
+		.when()
 			.get()
 		.then()
-			.body("", Matchers.hasSize(2))
-			.body("nome", Matchers.hasItems("Indiana", "Tailandesa"));
+			.body("", hasSize(2));
 	}
 	
 	@Test
 	public void deveRetornarStatus201_QuandoCadastrarCozinha() {
-		RestAssured.given()
-			.body("{\"nome\": \"Chinesa\" }")
+		given()
+			.body("{ \"nome\": \"Chinesa\" }")
 			.contentType(ContentType.JSON)
 			.accept(ContentType.JSON)
 		.when()
 			.post()
 		.then()
 			.statusCode(HttpStatus.CREATED.value());
+	}
+
+	@Test
+	public void deveRetornarRespostaEStatusCorretos_QuandoConsultarCozinhaExistente() {
+		given()
+			.pathParam("cozinhaId", 2)
+			.accept(ContentType.JSON)
+		.when()
+			.get("/{cozinhaId}")
+		.then()
+			.statusCode(HttpStatus.OK.value())
+			.body("nome", equalTo("Americana"));
+	}
+	
+	@Test
+	public void deveRetornarStatus404_QuandoConsultarCozinhaInexistente() {
+		given()
+			.pathParam("cozinhaId", 100)
+			.accept(ContentType.JSON)
+		.when()
+			.get("/{cozinhaId}")
+		.then()
+			.statusCode(HttpStatus.NOT_FOUND.value());
 	}
 	
 	private void prepararDados() {
